7e1a6e710d2dbf8a92ae7d367248781ff9f0ec92
Joe Jamsky <cba1067950@yahoo.com>
2019-05-23 22:02:26 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,17 +1,22 @@
 require 'pry'
 
-def count_elements(array)
-  # code goes here
-  hash = {}
-  c_array = array.map do |x|
-   num = array.count(x)
-   num
-  end
+# def count_elements(array)
+#   # code goes here
+#   hash = {}
+#   c_array = array.map do |x|
+#   num = array.count(x)
+#   num
+#   end
 
-  array.each_with_index do |x, i| 
-    hash[x] = c_array[i]
-  end 
+#   array.each_with_index do |x, i| 
+#     hash[x] = c_array[i]
+#   end 
   
-  hash
-end
- 
+#   hash
+# end
+
+def count_elements(array) 
+  count = Hash.new(0)
+  array.each {|item| count[item] += 1 }
+  count
+end 
